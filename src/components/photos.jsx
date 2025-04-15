ab415b81f2dc06674fda627ca11791a6bf2ff1bb
thianah <ekundayo6789@gmail.com>
2025-04-15 13:51:37 +0100

    updated

--- src/components/photos.jsx
+++ src/components/photos.jsx
@@ -1,23 +1,37 @@
-import photo1 from '../assets/photo1.jpg';
-import photo2 from '../assets/photo2.jpg';
-import photo3 from '../assets/photo3.jpg';
-import photo4 from '../assets/photo4.jpg';
-import React from 'react';
+import photo1 from "../assets/photo1.jpg";
+import photo2 from "../assets/photo2.jpg";
+import photo3 from "../assets/photo3.jpg";
+import photo4 from "../assets/photo4.jpg";
+import React from "react";
 
-function Photo () {
-    return(
-        <div id="photo">
-        <div className="photo">
-            <h1>our pictures</h1>
-            <img className='img' src={photo1} alt="Photo 1" />
-            <img  className='img' src={photo2} alt="Photo 2" />
-            <img className='img' src={photo3} alt="Photo 3" />
-            <img className='img' src={photo4} alt="Photo 4" />
-
-
+function Photo() {
+  return (
+    <div id="photo">
+      <div className="album">
+        <div className="image-container-bg"> {/* Matches CSS */}
+          <h1>Our Pictures</h1>
+          <div className="image-container-block"> {/* Matches CSS */}
+            <img className="img" src={photo1} alt="Photo 1" />
+            <img className="img" src={photo2} alt="Photo 2" />
+            <img className="img" src={photo3} alt="Photo 3" />
+            <img className="img img-last" src={photo4} alt="Photo 4" />
+          </div>
+          <div className="image-container-block"> {/* Matches CSS */}
+            <img className="img" src={photo1} alt="Photo 1" />
+            <img className="img img-big" src={photo2} alt="Photo 2" />
+            <img className="img" src={photo3} alt="Photo 3" />
+            <img className="img img-big img-last" src={photo4} alt="Photo 4" />
+          </div>
+          <div className="image-container-block"> {/* Matches CSS */}
+            <img className="img" src={photo1} alt="Photo 1" />
+            <img className="img" src={photo2} alt="Photo 2" />
+            <img className="img" src={photo3} alt="Photo 3" />
+            <img className="img" src={photo4} alt="Photo 4" />
+          </div>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Photo;
--- src/components/story.jsx
+++ src/components/story.jsx
@@ -2,73 +2,60 @@ function Story() {
   return (
     <div
       id="Story"
-      className="p-6 bg-gray-100 text-gray-800  min-h-screen flex flex-col items-center"
+      className="p-6 bg-gray-100 text-gray-800 min-h-screen flex flex-col items-center"
     >
-      <div className="story text-center max-w-4xl">
+      <div className="story text-center max-w-6xl"> {/* Adjust max width */}
         <h1 className="text-4xl font-bold mb-6 text-gray-900">
           OUR LOVE STORY
         </h1>
-        <div className="mt-4 bg-white p-6 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-semibold mb-4 text-gray-700 text-purple-950 ">BRIDE</h2>
-          <p className="text-lg leading-relaxed text-gray-600">
-            Abass and I went to the same secondary school, but that's not where
-            it all started from. We barely knew each other then because I
-            transferred to a different school.
-            <br />
-            Fastforward to a day in March 2023, and I had gone out to my
-            friend's place and taken a few pictures. Later, my sister who
-            happened to be friends with Abass at the time, uploaded one of my
-            photos on her WhatsApp status. That moment changed everything.
-            <br />
-            He saw the picture and messaged my sister and said, “My
-            wife is on your contact list.” My sister told me she found my
-            husband (smile) and then sent him my number and that was the beginning of everything.
-            <br />
-            From that one picture, one message, one call, a beautiful love story
-            unfolded. We’ve been together for two amazing years now, coupled
-            with the fact that it was a distance relationship, and what started
-            with a WhatsApp status is blossoming into a lifetime commitment.
-            <br />
-            It’s funny how love works. Sometimes, it waits for the right time to
-            show up, even if it means circling back to someone who was once just
-            a familiar face from the past.
-            <br />
-            I love him so much, and I can’t wait to spend the rest of my life with him.
-          </p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8"> {/* Adjust gap */}
+          <div className="mt-4 bg-white p-8 rounded-lg shadow-lg"> {/* Adjust padding */}
+            <h2 className="text-2xl font-semibold mb-4 text-gray-700 text-purple-950">
+              BRIDE
+            </h2>
+            <p className="text-lg leading-relaxed text-gray-600">
+              Abass and I went to the same secondary school, but that's not where
+              it all started from. We barely knew each other then because I
+              transferred to a different school.
+              <br />
+              Fastforward to a day in March 2023, and I had gone out to my
+              friend's place and taken a few pictures. Later, my sister who
+              happened to be friends with Abass at the time, uploaded one of my
+              photos on her WhatsApp status. That moment changed everything.
+              <br />
+              He saw the picture and messaged my sister and said, “My
+              wife is on your contact list.” My sister told me she found my
+              husband (smile) and then sent him my number and that was the beginning of everything.
+              
+            
+              <br />
+              I love him so much, and I can’t wait to spend the rest of my life with him.
+            </p>
           </div>
-        
-          <div className="mt-4 bg-white p-6 rounded-lg shadow-lg">
-          <h2 className="text-2xl font-semibold mb-4 text-purple-950">GROOM</h2>
-          <p className="text-lg leading-relaxed text-gray-600">
-            Abass and I went to the same secondary school, but that's not where
-            it all started from. We barely knew each other then because I
-            transferred to a different school.
-            <br />
-            Fastforward to a day in March 2023, and I had gone out to my
-            friend's place and taken a few pictures. Later, my sister who
-            happened to be friends with Abass at the time, uploaded one of my
-            photos on her WhatsApp status. That moment changed everything.
-            <br />
-            He saw the picture and messaged my sister and said, “My
-            wife is on your contact list.” My sister told me she found my
-            husband (smile) and then sent him my number and that was the beginning of everything.
-            <br />
-            From that one picture, one message, one call, a beautiful love story
-            unfolded. We’ve been together for two amazing years now, coupled
-            with the fact that it was a distance relationship, and what started
-            with a WhatsApp status is blossoming into a lifetime commitment.
-            <br />
-            It’s funny how love works. Sometimes, it waits for the right time to
-            show up, even if it means circling back to someone who was once just
-            a familiar face from the past.
-            <br />
-            I love him so much, and I can’t wait to spend the rest of my life with him.
-          </p>
-        
+          <div className="groom mt-4 bg-white p-8 rounded-lg shadow-lg"> {/* Adjust padding */}
+            <h2 className="text-2xl font-semibold mb-4 text-purple-950">
+              GROOM
+            </h2>
+            <p className="text-lg leading-relaxed text-gray-600">
+              Abass and I went to the same secondary school, but that's not where
+              it all started from. We barely knew each other then because I
+              transferred to a different school.
+              <br />
+              Fastforward to a day in March 2023, and I had gone out to my
+              friend's place and taken a few pictures. Later, my sister who
+              happened to be friends with Abass at the time, uploaded one of my
+              photos on her WhatsApp status. That moment changed everything.
+              <br />
+              He saw the picture and messaged my sister and said, “My
+              wife is on your contact list.” My sister told me she found my
+              husband (smile) and then sent him my number and that was the beginning of everything.
+              <br />
+              I love him so much, and I can’t wait to spend the rest of my life with him.
+            </p>
           </div>
         </div>
       </div>
-  
+    </div>
   );
 }
 
